1c509a4b6b62fc87f159a2363742b0e1a3b06f55
Your Name <you@example.com>
2023-01-17 17:24:04 -0500

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,84 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.DefaultDriveCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  // private final XboxController m_controller = new XboxController(0);
+  private final Joystick m_driver = new Joystick(0);
+
+  private final DrivetrainSubsystem m_drivetrainSubsystem;
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    m_drivetrainSubsystem = DrivetrainSubsystem.getInstance();
+
+    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(m_driver));
+    // Set up the default command for the drivetrain.
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    // m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+    // m_drivetrainSubsystem,
+    // () -> -modifyAxis(m_controller.getLeftY()) *
+    // DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+    // () -> -modifyAxis(m_controller.getLeftX()) *
+    // DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+    // () -> -modifyAxis(m_controller.getRightX()) *
+    // DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+    // ));
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // return m_drivetrainSubsystem.traj(PathPlanner.loadPath("Our Path", new PathConstraints(4, 3)), true);
+    //return m_drivetrainSubsystem.traj(PathPlanner.loadPath("Left 7 Path", new PathConstraints(4, 3)), true);
+    //return m_drivetrainSubsystem.traj(PathPlanner.loadPath("Angle Path", new PathConstraints(4, 3)), true);
+    return m_drivetrainSubsystem.traj(PathPlanner.loadPath("TestPath2", new PathConstraints(1, 1)), true);
+    
+    // An ExampleCommand will run in autonomous
+    // return m_autoCommand;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,279 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.Pigeon2;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter; 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Module;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+    // private static DrivetrainSubsystem instance = null;
+
+    double wheelBase = 31.625 * 0.0254;
+    // Locations for the swerve drive modules relative to the robot center.
+    Translation2d m_frontLeftLocation = new Translation2d(wheelBase, wheelBase);
+    Translation2d m_frontRightLocation = new Translation2d(wheelBase, -wheelBase);
+    Translation2d m_backLeftLocation = new Translation2d(-wheelBase, wheelBase);
+    Translation2d m_backRightLocation = new Translation2d(-wheelBase, -wheelBase);
+
+    // Creating my kinematics object using the module locations
+    SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+            m_frontLeftLocation,
+            m_frontRightLocation,
+            m_backLeftLocation,
+            m_backRightLocation);
+
+    // Creating my odometry object from the kinematics object. Here,
+    // our starting pose is 5 meters along the long end of the field and in the
+    // center of the field along the short end, facing forward.
+    SwerveDriveOdometry m_odometry;
+
+    Pose2d m_pose;
+
+    Joystick js0 = new Joystick(0);
+
+
+    private final double FRONT_LEFT_ENC_OFFSET = 322.82;
+    private final double FRONT_RIGHT_ENC_OFFSET = 339.87;
+    private final double BACK_RIGHT_ENC_OFFSET = 122.52;
+    private final double BACK_LEFT_ENC_OFFSET = 153.10;   
+
+    Module frontLeftModule = new Module("FrontLeft", 45, 40, 50, FRONT_LEFT_ENC_OFFSET);
+    Module frontRightModule = new Module("FrontRight", 43, 44, 51, FRONT_RIGHT_ENC_OFFSET);
+    Module backRightModule = new Module("BackRight", 23, 41, 52, BACK_RIGHT_ENC_OFFSET);
+    Module backLeftModule = new Module("BackLeft", 20, 22, 53, BACK_LEFT_ENC_OFFSET);
+
+    ShuffleboardTab drivetrainTab;
+
+    SlewRateLimiter fwdBakRateLimiter = new SlewRateLimiter(0.5);
+    SlewRateLimiter leftRightRateLimiter = new SlewRateLimiter(0.5);
+    SlewRateLimiter turnRateLimiter = new SlewRateLimiter(0.5);
+
+    Pigeon2 pigeon = new Pigeon2(13, "CANivore1");
+    //Global variable for drive rate speed
+    double g_driveRate;
+
+    public Rotation2d getGyroHeading() {
+        // // Get my gyro angle. We are negating the value because gyros return positive
+        // // values as the robot turns clockwise. This is not standard convention that
+        // is
+        // // used by the WPILib classes.
+        // var gyroAngle = Rotation2d.fromDegrees(-m_gyro.getAngle());
+        return Rotation2d.fromDegrees(pigeon.getYaw());
+    }
+
+    private Pose2d getPose() {
+        return m_odometry.getPoseMeters();
+    }
+
+    private static DrivetrainSubsystem m_inst = null;
+
+    public static DrivetrainSubsystem getInstance() {
+        if (m_inst == null) {
+            m_inst = new DrivetrainSubsystem();
+        }
+        return m_inst;
+    }
+    private SwerveModulePosition[] getModulePositions () {
+        SwerveModulePosition[] positions = new SwerveModulePosition[4];
+        positions[0] = frontLeftModule.getPosition();  
+        positions[1] = frontRightModule.getPosition();  
+        positions[2] = backRightModule.getPosition();
+        positions[3] = backLeftModule.getPosition();  
+       return positions;
+        
+    }
+    private DrivetrainSubsystem() {
+        pigeon.setYaw(0);
+
+        m_odometry = new SwerveDriveOdometry(
+                m_kinematics,
+                getGyroHeading(),
+             getModulePositions(),
+                
+                new Pose2d(0, 0, new Rotation2d()));
+        
+        SmartDashboard.putBoolean("Done", false);
+    }
+
+    // private DrivetrainSubsystem() {
+    // drivetrainTab = Shuffleboard.getTab("drivetrain");
+
+    // }
+
+    // public static DrivetrainSubsystem getInstance() {
+    // if (instance == null) {
+    // instance = new DrivetrainSubsystem();
+    // }
+    // return instance;
+    // }
+
+    // public ShuffleboardTab getTab() {
+    // return drivetrainTab;
+    // }
+
+    // this is alled every loop of the scheduler (~20ms)
+    @Override
+    public void periodic() {
+        frontLeftModule.periodic();
+        frontRightModule.periodic();
+        backLeftModule.periodic();
+        backRightModule.periodic();
+       
+        SmartDashboard.putNumber("Pidgeon yaw", pigeon.getYaw());
+
+        // Update the pose
+        // button 8 on xbox is three lines button
+        if (js0.getRawButton(8)) {
+            frontLeftModule.resetTurnEncoders();
+            frontRightModule.resetTurnEncoders();
+            backRightModule.resetTurnEncoders();
+            backLeftModule.resetTurnEncoders();
+        }
+        // button 7 on xbox it two squares
+        if (js0.getRawButton(7)) {
+            pigeon.setYaw(0);
+        }
+        //Button 6 is right bumper/slow button
+        //Speed is multiplied by 0.3 when held down
+        if (js0.getRawButton(6)) {
+            setDriveRate(0.3);
+        }
+        else {
+            setDriveRate(1);
+        }
+
+        SmartDashboard.putString("odo", m_odometry.getPoseMeters().toString());
+        SmartDashboard.putString("odo", m_odometry.getPoseMeters().toString());
+        // SmartDashboard.putNumber("Pose roation", m_pose.getRotation().getDegrees()) ;
+        // SmartDashboard.putNumber("Pose X", m_pose.getX());
+        // SmartDashboard.putNumber("Pose Y", m_pose.getY());
+
+        // DriveWithJoystick(js0);
+        m_odometry.update(getGyroHeading(), getModulePositions());
+    }
+
+    public static double convertTicksToDegrees(double ticks) {
+        // Cancoder: 2048 ticks per rotation
+        // Steering gear ratio: 150/7:1
+        double degrees = ticks * (1.0 / 2048.0) * (1.0 / (150 / 7)) * (360.0 / 1.0);
+        return degrees;
+    }
+
+    public static double convertDegreesToTicks(double degrees) {
+
+        double ticks = degrees * 1 / ((1.0 / 2048.0) * (1.0 / (150 / 7)) * (360.0 / 1.0));
+        return ticks;
+    }
+
+    public void DriveWithJoystick(Joystick js) {
+
+        double leftRightDir = -1 * getDriveRate() * js.getRawAxis(0); // positive number means left
+        double fwdBackDir = -1 * getDriveRate() * js.getRawAxis(1); // positive number means fwd
+        double turn = -1 * getDriveRate() * js.getRawAxis(4); // positive number means clockwise
+
+        // fwdBackDir = fwdBakRateLimiter.calculate(fwdBackDir);
+        // leftRightDir = leftRightRateLimiter.calculate(leftRightDir);
+        // turn = turnRateLimiter.calculate(turn);
+
+        double deadband = .05;
+
+        if (-deadband < leftRightDir && leftRightDir < deadband) {
+            leftRightDir = 0;
+        }
+
+        if (-deadband < fwdBackDir && fwdBackDir < deadband) {
+            fwdBackDir = 0;
+        }
+
+        if (-deadband < turn && turn < deadband) {
+            turn = 0;
+        }
+
+        // Example chassis speeds: 1 meter per second forward, 3 meters
+        // per second to the left, and rotation at 1.5 radians per second
+        // counteclockwise.
+        // ChassisSpeeds speeds = new ChassisSpeeds(fwdBackDir, leftRightDir, turn);
+
+        
+
+        ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+                fwdBackDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                leftRightDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                turn * DrivetrainSubsystem.MAX_OMEGA_RADIANS_PER_SECOND,
+                Rotation2d.fromDegrees(pigeon.getYaw()));
+
+        // Convert to module states
+        SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(speeds);
+
+        setModuleStates(moduleStates);
+
+    }
+
+    public static final double MAX_VELOCITY_METERS_PER_SECOND = 4;
+    public static final double MAX_OMEGA_RADIANS_PER_SECOND = 2.5;
+
+    public void setModuleStates(SwerveModuleState[] moduleStates) {
+
+        SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, MAX_VELOCITY_METERS_PER_SECOND);
+
+        SwerveModuleState frontLeft = moduleStates[0];
+        SwerveModuleState frontRight = moduleStates[1];
+        SwerveModuleState backRight = moduleStates[3];
+        SwerveModuleState backLeft = moduleStates[2];
+
+        frontLeftModule.setModuleState(frontLeft);
+        frontRightModule.setModuleState(frontRight);
+        backRightModule.setModuleState(backRight);
+        backLeftModule.setModuleState(backLeft);
+    }
+
+    public Command traj(PathPlannerTrajectory traj, boolean isFirstPath) {
+
+        PIDController xyController = new PIDController(5, 0, 0);
+
+        return new SequentialCommandGroup(
+                new InstantCommand(() -> {
+                    if (isFirstPath)
+                        this.m_odometry.resetPosition(getGyroHeading(),getModulePositions(),traj.getInitialHolonomicPose());
+            
+                }),
+                new PPSwerveControllerCommand(
+                        traj,
+                        this::getPose,
+                        this.m_kinematics,
+                        xyController,
+                        xyController,
+                        new PIDController(5, 0, 0),
+                        this::setModuleStates,
+                        this),
+                new InstantCommand(() -> {
+                    SmartDashboard.putBoolean("Done", true);
+                }));
+    }
+
+    public void setDriveRate(double driveRate) {
+        g_driveRate = driveRate;
+    }
+    public double getDriveRate() {
+        return g_driveRate;
+    }
+
+}
